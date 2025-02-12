87fb79af026a05b3fb3347dc6c512676211a6ef4
Lucas Bernardo <lbernardo.brito@gmail.com>
2025-02-12 15:54:07 -0300

    chore: add new possibilities for config files

--- config/module.go
+++ config/module.go
@@ -13,13 +13,27 @@ var ConfigModule = fx.Module("liquor-config", fx.Provide(
 	readConfigFile,
 ))
 
-func readConfigFile() *Config {
+func readConfigFile() (*Config, error) {
 	vp := viper.New()
-	vp.SetConfigFile("config.yaml")
-	vp.AutomaticEnv()
-	if err := vp.ReadInConfig(); err != nil {
-		fmt.Printf("failed to read config file: %v\n", err)
-		os.Exit(1)
+	files := []string{
+		"config.yaml",
+		"/etc/secrets/config.yaml",
 	}
-	return &Config{stg: vp}
+	var file string
+	for _, f := range files {
+		if _, err := os.Stat(f); !os.IsNotExist(err) {
+			file = f
+			break
+		}
+	}
+	if file == "" {
+		return nil, fmt.Errorf("config file not found try: [%v]", files)
+	}
+
+	vp.AutomaticEnv()
+	vp.SetConfigFile(file)
+	if err := vp.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+	return &Config{stg: vp}, nil
 }
